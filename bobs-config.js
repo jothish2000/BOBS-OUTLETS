990c938b6d8444135b481836609fe888f21e4a56
jothish2000 <jothish2000@gmail.com>
2026-09-04 19:27:04 +0530

    Correct Data Vault URL and retain flow version

--- bobs-config.js
+++ bobs-config.js
@@ -3,7 +3,7 @@
  * Browser storage is only a temporary UI cache.
  */
 window.BOBS_CONFIG = Object.freeze({
-  DATA_VAULT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxfxZLubLTNdW7jIFepJuRhz02Sch8WDQP4wQPeH38jV80LH-G2Y0tReJ6cWVjrcGQKpq/exec',
+  DATA_VAULT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxfxZLubLTNdW7jIFepJuRhz02Sch8WDQP4wQPeH38jV80LH-G2Y0tReJ6cWVjrcGQkPQ/exec',
   SHEETS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxhGWezXpQy5VBuQ7FDRuTntHFiZjHm5BkEIXUwFppW1w82mw955vV2zGPwkF3wXUb2ww/exec',
   VERSION: '2026-09-04-phase1-method-flow-3'
 });
