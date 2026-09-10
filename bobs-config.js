cebe7ffe3222ecfeabde60643f3f49afcd22185b
jothish2000 <jothish2000@gmail.com>
2026-09-10 23:43:08 +0530

    Fix Data Vault Web App URL to deployed endpoint

--- bobs-config.js
+++ bobs-config.js
@@ -1,8 +1,8 @@
 /* BOBS central configuration. Google Sheets and Data Vault are authoritative. */
 window.BOBS_CONFIG = Object.freeze({
   SHEETS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxhGWezXpQy5VBuQ7FDRuTntHFiZjHm5BkEIXUwFppW1w82mw955vV2zGPwkF3wXUb2ww/exec',
-  DATA_VAULT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxfxZLubLTNd7WjIFepJuRhz02Sch8WDQP4wQPeH38jv80LH-G2Y0tReJ6cWVjrcGQkPQ/exec',
-  VERSION: '2026-09-09-jsonp-read-transport'
+  DATA_VAULT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxfxZLubLTNd7WjIFepJuRhz02Sch8WDQP4wQPeH38jV80LH-G2Y0tReJ6cWVjrcGQkPQ/exec',
+  VERSION: '2026-09-10-data-vault-url-correction'
 });
 
 /* GitHub Pages cannot read Google Apps Script JSON with fetch because the
